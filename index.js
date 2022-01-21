520adce1489acb10b8dabc4c39daabd48bf77c10
Aiden <amb@aspy.dev>
2022-01-21 12:29:03 -0500

    Holy Fuck it actually works
    Manually committing this because I fucked the repo

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ export const ownerID="516750892372852754";//Set this to your own ID.
 export const devMode=true;//Set this to true if you want to run the bot in dev mode.
 export const guildID="774050236376285224";//Set this to the guild ID. This is only used if you want to run the bot in dev mode.
 console.log(`Attempting to start bot...\nOwner ID: ${ownerID}\nAttempting to load dependencies...`);
-import{Client,Intents,Collection}from'discord.js';
+import{Client,Intents}from'discord.js';
 import dotenv from'dotenv';
 import{readdirSync}from'fs';
 import{pushCommands}from'./pushCommands.js';
@@ -17,30 +17,26 @@ export const client=new Client({intents:[Intents.FLAGS.GUILDS]});
 dotenv.config();
 console.log('Found .env file.\nSearching for commands...');
 export let commandData=[];
-export let commands=new Collection();
+export let commands=new Map();
 const commandFolders=readdirSync('./commands/');
 for(const folder of commandFolders){
     const commandFiles=readdirSync(`./commands/${folder}`).filter((file)=>file.endsWith(".js"));
     for(const file of commandFiles){
-        let command=import(`./commands/${folder}/${file}`).then((command)=>{
-            commandData.push(command.data.toJSON());
-            commands.set(command.name, command);
-            console.log(command);
-            console.log(command.execute);
-            console.log(command.data.name);
-        });
+        const command=await import(`./commands/${folder}/${file}`)
+        commandData.push(command.data.toJSON());
+        commands.set(command.data.name,command);
     };
 };
-console.log(commandData.length);
+await pushCommands();
 console.log(`Found ${commandData.length} commands.\nWaiting on Discord API...`);
-client.once('ready',()=>console.log(`Connected to Discord API!\nLogged in as ${client.user.tag}.\nMy ID: ${client.user.id}\nGuild Count: ${client.guilds.size}`));
+client.once('ready',()=>console.log(`Connected to Discord API!\nLogged in as ${client.user.tag}.\nMy ID: ${client.user.id}\nGuild Count: ${client.guilds.cache.size}`));
 client.on('interactionCreate',(interaction)=>{
-    console.log(interaction);
-    if (!interaction.isCommand())return;
+    if(!interaction.isCommand())return;
     try{
-        commands.execute(interaction);
+        commands.get(interaction.commandName).execute(interaction);
+        console.log(`Executed command: ${interaction.commandName}`);
     }catch(e){
         console.log(e);
     };
 });
-client.login(process.env.DISCORD_TOKEN);
+await client.login(process.env.DISCORD_TOKEN);
